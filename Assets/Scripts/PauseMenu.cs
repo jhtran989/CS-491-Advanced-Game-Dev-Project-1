15580722dd85fff9ef9abe8249365fc73b3487ee
John Tran <jtran989@unm.edu>
2023-02-19 11:53:25 -0700

    Forgot to add the pause menu code

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public bool checkGameIsPaused = gameIsPaused;
+
+    public GameObject pauseMenuUI;
+    private static bool initialCreation = true;
+    
+    // Mimic deletion of objects created on new load (from using DontDestroyOnLoad) since the Canvas and ALL children are affected...including the Pause Menu
+    private void Awake()
+    {
+        if (initialCreation)
+        {
+            initialCreation = false;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // FIXME: cannot see static variables in Inspector so use a public variable that matches gameIsPaused
+        checkGameIsPaused = gameIsPaused;
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        
+        //Debug.Log("pause - successful?");
+    }
+
+    public void LoadMenu()
+    {
+        // FIXME: uncomment when doing TODO below
+        // Time.timeScale = 1f;
+        Debug.Log("Check Menu Button");
+        
+        // TODO: create menu scene (separate from Main Scene) to load
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Check Quit Button");
+        Application.Quit();
+    }
+}
